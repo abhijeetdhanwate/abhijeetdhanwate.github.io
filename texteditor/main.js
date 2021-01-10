c2ca0e5eda75bfab1cd96acf182c31698c37b153
Abhijeet Dhanwate <62416129+coderAbhii@users.noreply.github.com>
2021-01-10 23:00:36 +0530

    Update main.js

--- texteditor/main.js
+++ texteditor/main.js
@@ -7,3 +7,11 @@ function enableDesignMode(){
 function execCmd(command, arg=null){
     editor.document.execCommand(command, false, arg);
 }
+
+function onSignIn(googleUser) {
+  var profile = googleUser.getBasicProfile();
+  console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
+  console.log('Name: ' + profile.getName());
+  console.log('Image URL: ' + profile.getImageUrl());
+  console.log('Email: ' + profile.getEmail()); // This is null if the 'email' scope is not present.
+}
